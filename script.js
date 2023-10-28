c47922258b95e2b5199b9d724514e2225cb18d94
saloum45 <saloumfall45@gmail.com>
2023-10-28 13:53:41 +0000

    ajout de deux nouvelle questions

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ const questions = [
     correctAnswer: 3,
   },
   {
-    question: "Quelles sont les deux langues officielles du Canada ? (choix multiple)",
+    question:
+      "Quelles sont les deux langues officielles du Canada ? (choix multiple)",
     options: ["Français", "Anglais", "Espagnol", "Allemand"],
     correctAnswer: [0, 1],
   },
@@ -27,10 +28,14 @@ const questions = [
   },
   {
     question: "Quel est l'objectif principal du Hacktoberfest ?",
-    options: ["Célébrer Halloween", "Promouvoir la consommation de citrouilles", "Encourager la contribution à des projets open source"],
+    options: [
+      "Célébrer Halloween",
+      "Promouvoir la consommation de citrouilles",
+      "Encourager la contribution à des projets open source",
+    ],
     correctAnswer: 2,
-   },
-   {
+  },
+  {
     question: "Quelle societe a cree le hacktoberfest ?",
     options: ["DigitalOcean", "Microsoft", "galsenDev", "xarala"],
     correctAnswer: 0,
@@ -40,12 +45,22 @@ const questions = [
     options: ["1 ans", "1 mois", "1 week-end"],
     correctAnswer: 1,
   },
+  {
+    question: "Il reste combien de jours pour valider le Hacktoberfest actuel ?",
+    options: [31 - new Date().getDate(), "2", "8", "20"],
+    correctAnswer: 0,
+  },
+  {
+    question: "En qu'elle année le Hacktoberfest a commencé ?",
+    options: ["2013", "2015","2018", "2010"],
+    correctAnswer: 0,
+  },
 ];
 
 let currentQuestion = 0;
 let score = 0;
-let correctAnswers = 0
-let numberOfClick = 0
+let correctAnswers = 0;
+let numberOfClick = 0;
 
 const questionElement = document.getElementById("question");
 const optionsElement = document.getElementById("options");
@@ -59,7 +74,9 @@ function loadQuestion() {
   question.options.forEach((option, index) => {
     const li = document.createElement("li");
     li.textContent = option;
-    li.addEventListener("click", function () { return checkAnswer(index, this) });
+    li.addEventListener("click", function () {
+      return checkAnswer(index, this);
+    });
     optionsElement.appendChild(li);
   });
 }
@@ -67,21 +84,20 @@ function loadQuestion() {
 function checkAnswer(selectedIndex, currentList) {
   const question = questions[currentQuestion];
 
-
   if (Array.isArray(question.correctAnswer)) {
-    numberOfClick++
+    numberOfClick++;
     currentList.classList.add("active");
 
     if (question.correctAnswer.includes(selectedIndex)) {
-      correctAnswers++
+      correctAnswers++;
     }
     if (numberOfClick === question.correctAnswer.length) {
       if (correctAnswers === question.correctAnswer.length) {
-        score++
-        scoreElement.innerText = score
+        score++;
+        scoreElement.innerText = score;
       }
-      numberOfClick = 0
-      correctAnswers = 0
+      numberOfClick = 0;
+      correctAnswers = 0;
       currentQuestion++;
       if (currentQuestion < questions.length) {
         loadQuestion();
@@ -91,12 +107,12 @@ function checkAnswer(selectedIndex, currentList) {
         scoreElement.textContent = score;
       }
     }
-    return
+    return;
   }
 
   if (selectedIndex === question.correctAnswer) {
     score++;
-    scoreElement.innerText = score
+    scoreElement.innerText = score;
   }
 
   currentQuestion++;
